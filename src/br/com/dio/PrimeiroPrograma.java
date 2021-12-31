3ee64e5a0178fa169fa55b3b9155188f0f7cb27f
jultty <juffff@tutanota.de>
2021-12-31 00:26:59 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -11,10 +11,49 @@ public class PrimeiroPrograma {
         System.out.println(gato);
         System.out.println(neguinha);
         System.out.println(bethania);
-/*
+
+        Livro livro1 = new Livro("O Problema dos 3 Copos", 300);
+
+        System.out.println(livro1);
+
+        /*
         int a = 5;
         int b = 3;
         System.out.println("Olá, Dio! " + (a+b));
-*/
+        */
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
